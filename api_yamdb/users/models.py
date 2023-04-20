3e309d24c79af33fd7c282e61ceb0a2548e50c3f
Рустам Керимов <r.s.kerimov@yandex.ru>
2023-04-20 11:07:46 +0300

    Создана модель пользователя

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class RoleUser(AbstractUser):
+    CHIOCE_ROLE = (
+        ('U', 'User'),
+        ('M', 'Moderator'),
+        ('A', 'Admin'),
+    )
+    role = models.CharField(verbose_name='Пользовательская роль',
+                            max_length=1, choices=CHIOCE_ROLE, default='U')
+    bio = models.TextField(verbose_name='Биография', blank=True,
+                           max_length=250)
